9f20e37459a0c006c7689768dce1973aa989c120
kaushikashok45 <56879395+kaushikashok45@users.noreply.github.com>
2021-09-19 23:54:28 +0530

    Update settings.py

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -135,7 +135,7 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR/home/static 
+STATIC_ROOT = home/static 
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
